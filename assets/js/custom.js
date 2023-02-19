11db13a65c4e3e2818e1aaf36d05b021fc3f1bd5
fatemeh salarian <ftme_slr@yahoo.com>
2023-02-19 18:28:30 +0330

    fix another dropdaown data

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -123,3 +123,118 @@ function submitForCalculateWeight() {
     document.getElementById("weight").innerHTML = weight.toFixed(2);
   }
 }
+function getOption() {
+  selectElement = document.getElementById("select1");
+  output = selectElement.value;
+  console.log(output);
+
+  switch (output) {
+    case "76.0":
+      document.getElementById("test1").innerHTML = "80";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "80.7":
+      document.getElementById("test1").innerHTML = "85";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "85.5":
+      document.getElementById("test1").innerHTML = "90";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "90.2":
+      document.getElementById("test1").innerHTML = "95";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "96.0":
+      document.getElementById("test1").innerHTML = "101";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "102":
+      document.getElementById("test1").innerHTML = "107";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "107":
+      document.getElementById("test1").innerHTML = "113";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "114":
+      document.getElementById("test1").innerHTML = "120";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "121":
+      document.getElementById("test1").innerHTML = "127";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "127":
+      document.getElementById("test1").innerHTML = "134";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "135":
+      document.getElementById("test1").innerHTML = "142";
+      document.getElementById("test2").innerHTML = "-";
+      break;
+
+    case "143":
+      document.getElementById("test1").innerHTML = "150";
+      document.getElementById("test2").innerHTML = "0.4";
+      break;
+    case "150":
+      document.getElementById("test1").innerHTML = "158";
+      document.getElementById("test2").innerHTML = "2.5";
+      break;
+    case "143":
+      document.getElementById("test1").innerHTML = "1231";
+      document.getElementById("test2").innerHTML = "123sad1";
+      break;
+    case "158":
+      document.getElementById("test1").innerHTML = "175";
+      document.getElementById("test2").innerHTML = "6.7";
+      break;
+    case "174":
+      document.getElementById("test1").innerHTML = "183";
+      document.getElementById("test2").innerHTML = "8.7";
+      break;
+    case "182":
+      document.getElementById("test1").innerHTML = "192";
+      document.getElementById("test2").innerHTML = "10.7";
+      break;
+    case "191":
+      document.getElementById("test1").innerHTML = "201";
+      document.getElementById("test2").innerHTML = "12.6";
+      break;
+    case "200":
+      document.getElementById("test1").innerHTML = "211";
+      document.getElementById("test2").innerHTML = "14.5";
+      break;
+    case "209":
+      document.getElementById("test1").innerHTML = "220";
+      document.getElementById("test2").innerHTML = "16.4";
+      break;
+    case "219":
+      document.getElementById("test1").innerHTML = "230";
+      document.getElementById("test2").innerHTML = "18.2";
+      break;
+    case "228":
+      document.getElementById("test1").innerHTML = "240";
+      document.getElementById("test2").innerHTML = "20.3";
+      break;
+    case "238":
+      document.getElementById("test1").innerHTML = "250";
+      document.getElementById("test2").innerHTML = "22.2";
+      break;
+
+    default:
+      document.getElementById("test1").innerHTML = "-";
+      document.getElementById("test2").innerHTML = "-";
+  }
+}
