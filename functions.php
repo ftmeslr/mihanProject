903330c59aa47fb2059404ded9b6dc265783b23e
m.dehqany <m.dehqany@gmail.com>
2023-03-05 10:18:51 +0330

    Functions edit

--- functions.php
+++ functions.php
@@ -53,5 +53,45 @@ add_action("pre_get_posts",function($query){
     if(($query->is_category() || $query->is_tag()) && $query->is_main_query()){
         $query->set("post_type",array("post","articles","news"));
     }
-})
+});
+//Registe Attributes
+add_action("init",function(){
+    if(wc_attribute_taxonomy_id_by_name("state") == 0){
+        wc_create_attribute(array(
+            'slug'         => 'state',
+            'name'         =>'حالت',
+            'type'         => 'select',
+            'order_by'     => 'menu_order',
+        ));
+    } if(wc_attribute_taxonomy_id_by_name("standard") == 0){
+        wc_create_attribute(array(
+            'slug'         => 'standard',
+            'name'         =>'استاندارد',
+            'type'         => 'select',
+            'order_by'     => 'menu_order',
+        ));
+    } if(wc_attribute_taxonomy_id_by_name("dimen") == 0){
+        wc_create_attribute(array(
+            'slug'         => 'dimen',
+            'name'         =>'ابعاد',
+            'type'         => 'select',
+            'order_by'     => 'menu_order',
+        ));
+    } if(wc_attribute_taxonomy_id_by_name("size") == 0){
+        wc_create_attribute(array(
+            'slug'         => 'size',
+            'name'         =>'سایز',
+            'type'         => 'select',
+            'order_by'     => 'menu_order',
+        ));
+    } if(wc_attribute_taxonomy_id_by_name("tyeps") == 0){
+        wc_create_attribute(array(
+            'slug'         => 'tyeps',
+            'name'         =>'نوع',
+            'type'         => 'select',
+            'order_by'     => 'menu_order',
+        ));
+    }
+    //delete_transient( 'wc_attribute_taxonomies' );
+});
 ?>
